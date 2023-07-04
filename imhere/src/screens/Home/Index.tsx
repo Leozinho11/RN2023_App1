f46854eb1d33b14273e66b66c3cc417d7c6d3567
Leozinho11 <leoferraribarreto@hotmail.com>
2023-07-04 20:50:51 -0300

    Commit 1 -- Create the repository of the first app

---
+++ imhere/src/screens/Home/Index.tsx
@@ -0,0 +1,41 @@
+import { Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { styles } from './styles';
+import { Participant } from '../../components/Participant';
+
+export function Home() {
+
+  function handleParticipantAdd() {
+    console.log("Você clicou no botão de adicionar")
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
+        Nome do evento
+      </Text>
+
+
+      <Text style={styles.eventDate}>
+        Sexta, 4 de Novembro de 2022
+      </Text>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder='Nome do participante'
+          placeholderTextColor='#6B6B6B'
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>
+            +
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Participant name="Rodrigo" />
+      <Participant name="Leo" />
+      <Participant name="Lola" />
+    </View>
+  )
+}
